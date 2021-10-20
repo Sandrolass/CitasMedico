6324df96361f0669a13ea4a396c4bf43a424caf1
MarMP <marmembriveplaza@gmail.com>
2021-10-20 09:46:02 +0200

    model usuario

---
+++ CitasMedico/src/app/models/usuario.ts
@@ -0,0 +1,23 @@
+export interface Medico {
+    _id: string,
+    refM: string,
+    nombre: string,
+    apellido: string,
+    dni: string,
+    fecha: Date
+}
+
+export interface UsuarioBasico {
+    _id?: string,
+    dni: string,
+    nombre: string,
+    apellido: string
+}
+
+export interface UsuarioCompleto {
+    _id: string, 
+    nombre: string, 
+    apellidos: string, 
+    dni: string, 
+    medico: Medico
+}
