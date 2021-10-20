7e9cb33428e90b44f057bc4ad70cb2ef04b357e0
jviilaa <javior@floridauniversitaria.es>
2021-10-20 08:50:36 +0200

    Lo mismo que el commit anterior corrigiendo errores

--- CitasMedico/src/app/services/usuario.service.ts
+++ CitasMedico/src/app/services/usuario.service.ts
@@ -1,11 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  urlBase = environment.urlBase + "usuario";
+
+  constructor(private http: HttpClient) { }
 
   loginUser(credentials: string): Observable<any> {
     let url = this.urlBase + credentials;
