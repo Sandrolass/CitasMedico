ccae209d7aaf8552fe8975017a342560caa8d1d9
ElectricSaltyFish <fake@gmail.com>
2021-10-20 11:52:37 +0200

    Calendario

--- CitasMedico/src/app/app.module.ts
+++ CitasMedico/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-import { CitaService } from './services/cita.service';
-import { CalendarioComponent } from './calendario/calendario.component';
-import { HttpClientModule } from '@angular/common/http';
-
-import { NotFoundComponent } from './not-found/not-found.component';
 import { MatButtonModule } from '@angular/material/button';
 import { DateAdapter, MatNativeDateModule, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS} from '@angular/material-moment-adapter';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
+import { CitaService } from './services/cita.service';
+import { CalendarioComponent } from './calendario/calendario.component';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
