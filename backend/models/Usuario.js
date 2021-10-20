bb2cacde9709d8b31c63fbc4a9297c870d54be68
Sandro <sandro@example.com>
2021-10-20 12:30:52 +0200

    backend terminado v1

--- backend/models/Usuario.js
+++ backend/models/Usuario.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let Usuario = new Schema({
-    nombre: {type:String},
-    apellidos: {type:String},
-    dni: {type:String},
-    medico:{type:String}
+    nombre: {type:String,required:true},
+    apellidos: {type:String,required:true},
+    dni: {type:String,required:true,unique:true},
+    medico:{type:String,required:true}
 },{collection:"usuarios"});
 
 
